62403afd01e0e8c3c90cd4d0c6f5b56373c33f0f
Riva Almero <rvalms@icloud.com>
2026-01-23 15:01:50 +0700

    feat: Initialize Nuxt 4 project with Nuxt UI and Tailwind CSS, including basic page structure and Node.js version update.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
    compatibilityDate: "2025-07-15",
    devtools: { enabled: true },
    modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxt/icon"],
+   css: ["@/assets/css/main.css"],
    fonts: {
       defaults: {
          weights: [400, 500, 600, 700, 800, 900],
