b2e98248b4e78ea1a58605176475a5bcc465b85d
Riva Almero <rvalms@icloud.com>
2026-01-23 14:39:28 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+   compatibilityDate: "2025-07-15",
+   devtools: { enabled: true },
+   modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxt/icon"],
+   fonts: {
+      defaults: {
+         weights: [400, 500, 600, 700, 800, 900],
+      }
+   }
+})
